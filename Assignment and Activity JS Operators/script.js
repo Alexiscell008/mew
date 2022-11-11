45c80d61a40fd45865e33a8542b7b18bbb71c902
aaconde <alexiscellconde@gmail.com.com>
2022-11-11 21:11:53 +0800

    Lectures

---
+++ Assignment and Activity JS Operators/script.js
@@ -0,0 +1,12 @@
+/*
+1. The comparison operator performs a comparison on its operands and returns a result that indicates whether or not the comparison was successful. The values of the operands can be represented as numbers, strings, logical expressions, or objects. The comparison of strings is done via Unicode value, and is based on the normal lexicographical ordering. If the two operands being compared are not of the same type, JavaScript will, in the majority of instances, make an effort to convert them to a type that is more suitable for the comparison.
+
+2. Boolean object represents a truth value: true or false
+
+3.  The Addition Assignment Operator (+=) adds a value to a variable. + is used to perform arithmetic on numbers. But both can be used to add (concatenate) strings.
+*/
+
+console.log("connected");
+
+
+
