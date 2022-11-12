866d1d2b1a35e6dbc76803d14076a2ab93a1e8fe
alexisconde <email>
2022-11-12 19:03:42 +0800

    Submit

--- Assignment and Activity Repetition Control Structures/script.js
+++ Assignment and Activity Repetition Control Structures/script.js
@@ -1,7 +1,59 @@
-public class Main {
-    public static void main(String[] args) {
-      for (int i = 0; i <= 10; i = i + 2) {
-        System.out.println(i);
-      }  
+console.log("connected");
+
+
+//ACTIVITY 1
+
+//Prompt a user to enter a number.
+let number = prompt ("Give me a number");
+
+for (number; number >= 0; number--)
+{
+   
+
+    if (number < 50){
+        console.log(`Currently at number ${number}. Terminating the loop`);
+        break; 
     }
-  }
+
+    // if (number % 10 === 0)
+    // {
+    //     console.log(`The number is divisible by 10. Skipping the number`);
+    //     continue;
+    // }
+
+
+    if (number % 5 === 0)
+    {
+        console.log(number);
+    }
+
+}
+
+
+//ACTIVITY 2
+
+//Prompt a user to enter a number.
+// let number = prompt ("Give me a number");
+
+// for (number; number >= 0; number--)
+// {
+   
+
+//     if (number < 50){
+//         console.log(`Currently at number ${number}. Terminating the loop`);
+//         break; 
+//     }
+
+    // if (number % 10 === 0)
+    // {
+    //     console.log(`The number is divisible by 10. Skipping the number`);
+    //     continue;
+    // }
+
+
+//     if (number % 5 === 0)
+//     {
+//         console.log(number);
+//     }
+
+// }  
--- s15/script.js
+++ s15/script.js
@@ -205,11 +205,11 @@ for (number; number >= 0; number--)
     }
 
     //When a number is divisible by 10, display a message that it is divisible by 10. Therefore, they will skip the number.
-    if (number % 10 === 0)
-    {
-        console.log(`The number is divisible by 10. Skipping the number`);
-        continue; //jumps over one iteration in the loop
-    }
+    // if (number % 10 === 0)
+    // {
+    //     console.log(`The number is divisible by 10. Skipping the number`);
+    //     continue; //jumps over one iteration in the loop
+    // }
 
     //If it is divisible by 5, display the number.
 
