37b468c10f40c618ced2927b3960d37803c1358d
acconde <114787853+acconde@users.noreply.github.com>
2023-01-04 07:08:13 +0800

    New

--- s27/src/Pages/Register.js
+++ s27/src/Pages/Register.js
@@ -1,90 +1,127 @@
 import {useState, useEffect} from 'react';
 import {
     Container,
-    Row,
-    Col,
     Form,
     Button
 } from 'react-bootstrap'
 
+import Swal from 'sweetalert2';
+
 export default function Register(){
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
-    const [cp, setCp] = useState("")
 
-    const [isActive, setIsActive] = useState(true)
+	const [email, setEmail] = useState("")
+	const [password, setPassword] = useState("")
+	const [cpw, setCpw] = useState("")
+    
+	const [isDisabled, setIsDisabled] = useState(true)
 
-    function registerUser(e) {
-        e.preventDefault()
+	useEffect( ()=>{
+		if((email !== "" && password !== "" && cpw !== "") && 
+			(password === cpw)){
+			setIsDisabled(false)
+		} else {
+			setIsDisabled(true)
+		}
 
-        setEmail("")
-        setPassword("")
-        setCp("")
-        alert("Thank you for registering to our Enrollment System")
-    }
+	}, [email, password, cpw])
 
-    //using useEffect, we will disable the button to prevent submitting while it is empty.
-    //allow the button to submit if input fields are not empty and passwords should match
 
-    useEffect( () => {
-        if ((email !== "" && password !== "") && (password === cp)){
-            setIsActive(false)
-        }
-    }, [email, password, cp])
+	function Register(e){
+		e.preventDefault()
 
-        return (
-            <Container className="my-5">
-                <Row className = "justify-content-center">
-                    <Col xs={10} md={6}>
-                        <Form
-                            className = "border p-3"
-                            onSubmit = {(e) => registerUser(e)}
-                        >
-                            {/*email*/}
-                            <Form.Group className = "mb-3" controlId="formBasicEmail">
-                                <Form.Label>Email Address</Form.Label>
-                                <Form.Control 
-                                    type="email"
-                                    placeholder = "Enter your email address"
-                                    value={email}
-                                    onChange = {(e) => {
-                                        setEmail (e.target.value)
-                                    }}
-                                />
-                            </Form.Group>
+		//check if the values are successfully bounded.
+		console.log(email);
+		console.log(password);
+		console.log(cpw);
+        
+        //send a request to our API collection
+        //apply an environment variable in our fetch request.
+		fetch(`localhost/api/users/register `, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({
+				email: email,
+				password: password
+			})
+		}).then(response => response.json()).then(convertedData => {
+			console.log(convertedData); 
 
-                            {/*password*/}
-                            <Form.Group className="mb-3" controlId="formBasicPassword">
-                                <Form.Label>Password</Form.Label>
-                                <Form.Control 
-                                    type="password" 
-                                    placeholder="Password"
-                                    value={password}
-                                    onChange={(e)=>{setPassword(e.target.value)}} />
-                            </Form.Group>
+			//create a control structure
+			if (convertedData === true) {
+				setEmail("")
+				setPassword("")
+				setCpw("")
+              //successful 
+              Swal.fire({
+              	title: 'Account Created Successfully',
+              	icon: 'success', 
+              	text: 'Congrats'
+              })
+			} else { 
+              //unsuccessful 
+              Swal.fire({
+              	title: 'Account Creation Failed',
+              	icon: 'error', 
+              	text: 'Something Went Wrong'
+              })
+			}
+		})
+		//how to handle a Promise? 3 states
+		// Pending => Fulfilled or Rejected
 
-                            {/*confirm password*/}
-                            <Form.Group className="mb-3" controlId="formBasicConfirmPassword">
-                                <Form.Label>Confirm Password</Form.Label>
-                                <Form.Control 
-                                    type="password" 
-                                    placeholder="Retype Password"
-                                    value={cp}
-                                    onChange={(e)=>{setCp(e.target.value)}} />
-                            </Form.Group>
 
-                            {/*confirm password*/}
-                            <Button 
-                                variant="primary" 
-                                type="submit"
-                                disabled={isActive}>
 
-                                    Submit
-                            </Button>
-                            
-                        </Form>
-                    </Col>
-                </Row>
-            </Container>
-        )
+		// alert("Registered Successfully!")
+	}
+
+	return(
+
+		<Container>
+			<Form 
+				className="border p-3 mb-3"
+				onSubmit={ (e) => Register(e) }
+				>
+			{/*email*/}
+			  <Form.Group className="mb-3" controlId="email">
+			    <Form.Label>Email address</Form.Label>
+			    <Form.Control 
+			    	type="email" 
+			    	placeholder="Enter email" 
+			    	value={email} 
+			    	onChange={ (e) => setEmail(e.target.value) }
+			    	/>
+			  </Form.Group>
+			{/*password*/}
+			  <Form.Group className="mb-3" controlId="password">
+			    <Form.Label>Password</Form.Label>
+			    <Form.Control 
+			    	type="password" 
+			    	placeholder="Password" 
+			    	value={password} 
+			    	onChange={ (e) => setPassword(e.target.value) }
+			    	/>
+			  </Form.Group>
+			{/*confirm password*/}
+			<Form.Group className="mb-3" controlId="cpw">
+			  <Form.Label>Verify Password</Form.Label>
+			  <Form.Control 
+			  		type="password" 
+			  		placeholder="Verify Password" 
+			  		value={cpw} 
+			  		onChange={ (e) => setCpw(e.target.value) }
+			  		/>
+			</Form.Group>
+			  
+			  <Button 
+			  		variant="primary" 
+			  		type="submit"
+			  		disabled={isDisabled}
+			  		>
+			    Submit
+			  </Button>
+			</Form>
+		</Container>
+	)
 }
